de8df4d0c14e1900f7690a9eb7b7de3b80921bd9
Rohit10jr <rohitjworkspace@gmail.com>
2024-05-13 16:36:08 +0530

    added comments and completed

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -33,7 +33,7 @@ load_dotenv(dotenv_path=env_path)
 user_api = os.environ['current_weather_data']
 # base_url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID={}"
 # PARAMS = {'units':'metric'}
-'''
+
 @login_required(login_url="/login")
 def city_detail(request):
     if request.method == 'POST':
@@ -45,9 +45,7 @@ def city_detail(request):
             new_form  = CityForm(request.POST)
 
             # cities = City.objects.filter(user=request.user).order_by('-id')[:5]
-            # cities = City.objects.filter(user=request.user).order_by('-created_at')[:5]
-            city_ids = City.objects.filter(user=request.user).order_by('-created_at').values_list('id', flat=True)[:5]
-            cities = City.objects.filter(id__in=city_ids)
+            cities = City.objects.filter(user=request.user).order_by('-created_at')[:5]
             weather_data = get_weather_data(cities)
             request.session['weather_data'] = weather_data
 
@@ -58,31 +56,29 @@ def city_detail(request):
     form = CityForm()
 
     # cities = City.objects.filter(user=request.user).order_by('-id')[:5]
-    cities = City.objects.filter(user=request.user).order_by('-created_at')[:5]
+    # city = City.objects.filter(user=request.user).order_by('-id')[:5]
     # cities = City.objects.filter(user=request.user).order_by('-created_at').values('name').distinct()[:5]
+    cities = City.objects.filter(user=request.user).order_by('-created_at')[:5]
     weather_data = get_weather_data(cities)
     request.session['weather_data'] = weather_data
-
     return render(request, 'main/home.html', {'cities': cities, 'form':form, 'weather_data' : weather_data})
 
 def get_weather_data(cities):
     weather_data = []  
+
+    print(cities)
 
     for city in cities:
         # url = base_url.format(city, user_api)
         url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'        
         url = url.format(city, user_api)
         PARAMS = {'units':'metric'}
-        # city_weather = requests.get(url).json()
-        # city_weather = requests.get(url).json()
         city_weather = requests.get(url,params=PARAMS).json()
-        # weather = 1
-        # print(city_weather)
 
         if 'cod' in city_weather and city_weather['cod'] == '404':
-            # City not found, handle this case
+            # City not found
             weather = {
-                'name' : city_weather.get('name', 'N/A'),
+                'name' : city.name,  
                 'lon': 'N/A',
                 'lat': 'N/A',
 
@@ -104,7 +100,7 @@ def get_weather_data(cities):
 
         elif 'weather' in city_weather and 'main' in city_weather:
             weather = {
-                'name' : city_weather.get('name', 'N/A'),  
+                'name' : city.name,    
                 # 'name' : city.name,  
                 'lon': city_weather['coord'].get('lon', 'N/A'),
                 'lat': city_weather['coord'].get('lat', 'N/A'),
@@ -146,137 +142,31 @@ def get_weather_data(cities):
                 'icon' : 'N/A'
             }
         weather_data.append(weather)
-        print(weather_data)
+        # print(weather_data)
         
     return weather_data
-'''
-# -----------------------------------------
 
-@login_required(login_url="/login")
-def city_detail(request):
-    cities = City.objects.filter(user=request.user).order_by('-id')[:5]
-    # cities = City.objects.filter(user=request.user).order_by('-created_at').values('name').distinct()[:5]
-    # cities = City.objects.filter(user=request.user).order_by('-id').values('name').distinct()[:5]
-    weather_data = []  
-
-    for city in cities:
-        # url = base_url.format(city, user_api)
-        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'        
-        url = url.format(city, user_api)
-        PARAMS = {'units':'metric'}
-        # city_weather = requests.get(url).json()
-        # city_weather = requests.get(url).json()
-        city_weather = requests.get(url,params=PARAMS).json()
-        # weather = 1
-        # print(city_weather)
-
-        if 'cod' in city_weather and city_weather['cod'] == '404':
-            # City not found, handle this case
-            weather = {
-                'name' : city.name,  
-                'lon': 'N/A',
-                'lat': 'N/A',
-
-                'temperature' : 'N/A',
-                'humidity' : 'N/A',
-                'pressure' : 'N/A',
-                'sea_level' : 'N/A',
-
-                'clouds': 'N/A', 
-                'temp_max' : 'N/A',
-                'temp_min' : 'N/A',
-                'wind_speed' : 'N/A',
-                'wind_deg' : 'N/A',
-
-                'country' : 'N/A',
-                'description' : 'City not found',
-                'icon' : 'N/A',
-            }
-
-        elif 'weather' in city_weather and 'main' in city_weather:
-            weather = {
-                'name' : city.name,  
-                'lon': city_weather['coord'].get('lon', 'N/A'),
-                'lat': city_weather['coord'].get('lat', 'N/A'),
-
-                'temperature' : city_weather['main'].get('temp', 'N/A'),
-                'humidity' : city_weather['main'].get('humidity', 'N/A'),
-                'pressure' : city_weather['main'].get('pressure', 'N/A'),
-                'sea_level' : city_weather['main'].get('sea_level', 'N/A'),
-
-                'clouds': city_weather['clouds'].get('all', 'N/A'),
-                'temp_max' : city_weather['main'].get('temp_max', 'N/A'),
-                'temp_min' : city_weather['main'].get('temp_min', 'N/A'),
-                'wind_speed' : city_weather['wind'].get('speed', 'N/A'),
-                'wind_deg' : city_weather['wind'].get('deg', 'N/A'),
-
-                'country' : city_weather['sys'].get('country', 'N/A'),
-                'description' : city_weather['weather'][0].get('description', 'City not found',),
-                'icon' : city_weather['weather'][0].get('icon', 'N/A'),
-            }
-        else:
-            weather = {
-                'name' : 'None',
-                'lon': 'N/A',
-                'lat': 'N/A',
-
-                'temperature' : 'N/A',
-                'humidity' : 'N/A',
-                'pressure' : 'N/A',
-                'sea_level' : 'N/A',
-                
-                'clouds': 'N/A', 
-                'temp_max' : 'N/A',
-                'temp_min' : 'N/A',
-                'wind_speed' : 'N/A',
-                'wind_deg' : 'N/A',
-                
-                'country' : 'N/A',
-                'description' : 'City not found',
-                'icon' : 'N/A'
-            }
-        weather_data.append(weather)
-        
-    if request.method == 'POST':
-        form  = CityForm(request.POST)
-        if form.is_valid():
-            todo = form.save(commit=False)
-            todo.user = request.user
-            todo.save()
-            return render(request, 'main/home.html', {'cities': cities, 'form':form, 'weather_data' : weather_data})
-    else:
-        form = CityForm()
-    # posts ={"greet": "hello"}
-    request.session['weather_data'] = weather_data
-    return render(request, 'main/home.html', {'cities': cities, 'form':form, 'weather_data' : weather_data})
-
+# update and plot for landing page
 
 def city_update(request, pk):
-    # weather_data = request.session.get('city_weather', None)
-    weather_data = request.session.get('weather_data', None)
-
-    print("-------update-------")
-    print(weather_data)
-    print("--------------------")
-
     city = get_object_or_404(City, id=pk, user=request.user)
-    print("--------city--------")
-    print(city)
-    print("--------------------")
+    if request.method == 'POST':
+        form = LandingForm(request.POST, instance=city)
+        if form.is_valid():
+            form.save()
+            return redirect('detail')
+        
+    weather_data = request.session.get('weather_data', None)
 
     city_weather_data = None
     for data in weather_data:
-        if data['name'] == city.name:
+        if data['name'] == city.name.lower():
             city_weather_data = data
-            print(city_weather_data)
+            # print(city_weather_data)
             break
     city_lon = city_weather_data['lon']
     city_lat = city_weather_data['lat']
-
-    print("-----lat and lon-----")
-    print(f'city_lat={city_lat} and city_lon={city_lon}')
-    print("----------------------")
-
+        
     # ---------open-meteo-config-----------
     cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
@@ -316,23 +206,8 @@ def city_update(request, pk):
         hourly_dataframe = pd.DataFrame(data = hourly_data)
         # print(hourly_dataframe)
 
-        '''
-        # Plot temperature
-        plt.plot(hourly_dataframe['date'], hourly_dataframe['temperature_2m'], label='Temperature (°C)', color='red')
-
-        # Plot relative humidity
-        plt.plot(hourly_dataframe['date'], hourly_dataframe['relative_humidity_2m'], label='Relative Humidity (%)', color='blue')
-
-        # Adding labels and title
-        plt.xlabel('Date and Time')
-        plt.ylabel('Value')
-        plt.title('Hourly Temperature and Relative Humidity')
-        plt.legend()
-        plt.grid(True)
-        '''
-
         # Styling parameters
-        temperature_color = '#FF0000'  # Orange
+        temperature_color = '#FF0000'   # Red
         humidity_color = '#4287f5'      # Blue
         background_color = '#f0f0f0'    # Light Gray
         grid_color = '#CCCCCC'          # Light Gray
@@ -371,28 +246,21 @@ def city_update(request, pk):
             "add", lambda sel: sel.annotation.set_text(f"{sel.artist.get_label()}: {sel.target[1]:.2f} %")
         )
 
-        # Rotate x-axis tick labels and reduce font size to avoid overlapping
+        # Rotate x-axis labels to avoid overlapping
         plt.tight_layout()
         plt.xticks(rotation=45, fontsize=10)
 
         plot_path = os.path.join(settings.MEDIA_ROOT, 'temp_plot.png')
-        
         plt.savefig(plot_path)
-    
-        if request.method == 'POST':
-            form = LandingForm(request.POST, instance=city)
-            if form.is_valid():
-                form.save()
-                return redirect('detail')
-        else:
-            form = LandingForm(instance=city)  # Pass the city instance to the form
-        return render(request, 'main/loading.html', {'form': form, 'city':city, 'weather_data':weather_data, 'plot_path': plot_path})  # 
-    
+        
     except Exception as e:
         # print(e)
-        return redirect('detail')
+        plot_path = os.path.join(settings.MEDIA_ROOT, '')
+    form = LandingForm(instance=city) 
+    return render(request, 'main/loading.html', {'form': form, 'city':city, 'weather_data':weather_data, 'plot_path': plot_path})  
 
 
+#  delete cities
 def city_delete(request, pk):
     city = get_object_or_404(City, id=pk, user=request.user)
     if request.method == 'POST':
@@ -400,85 +268,7 @@ def city_delete(request, pk):
     # return redirect('home')
     return redirect('detail')
 
-# -------------ploting graphs-------------
-
-def plot2(request):
-    url="https://archive-api.open-meteo.com/v1/archive?latitude=13.0878&longitude=80.2785&start_date=2024-04-15&end_date=2024-04-29&hourly=temperature_2m,relative_humidity_2m"
-    openmeteo_plot = requests.get(url).json()
-    return render(request, 'main/plot2.html', {'openmeteo_plot': openmeteo_plot})
-
-def plot_graph(request):
-    # Sample data (replace with your actual data)
-    days = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
-    temperature = [20, 22, 23, 25, 24, 22, 21]
-    humidity = [50, 55, 60, 65, 70, 75, 80]
-
-    # Create a dataframe from the data
-    data = {'Day': days, 'Temperature': temperature, 'Humidity': humidity}
-    df = pd.DataFrame(data)
-
-    # Plot the graph
-    plt.figure(figsize=(10, 6))
-    plt.plot(df['Day'], df['Temperature'], marker='o', label='Temperature (°C)')
-    plt.plot(df['Day'], df['Humidity'], marker='o', label='Humidity (%)')
-    plt.title('Temperature and Humidity Variation Over 7 Days')
-    plt.xlabel('Day')
-    plt.ylabel('Value')
-    plt.xticks(rotation=45)
-    plt.legend()
-    plt.grid(True)
-
-    # Save the plot to a temporary file
-    # plot_path = 'weatherapp/static/temp_plot.png'
-    # plot_path = os.path.join(settings.STATIC_URL, 'temp_plot.png')
-    # plt.savefig(plot_path)
-
-    plot_path = os.path.join(settings.MEDIA_ROOT, 'temp_plot.png')
-    # plt.savefig(plot_path)
-
-    # Save the plot to a temporary file
-    # plot_path = os.path.join(settings.STATIC_ROOT, 'temp_plot.png')
-    plt.savefig(plot_path)
-
-    # Pass the plot path to the template
-    return render(request, 'main/plot.html', {'plot_path': plot_path})
-
-
-''' map view '''
-
-def map(request):
-    # mymap = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
-
-    # Add a marker to the map
-    # folium.Marker(
-    #     location=[37.7749, -122.4194],
-    #     popup="San Francisco",
-    #     icon=folium.Icon(color="blue")
-    # ).add_to(mymap)
-    cities=[]
-    city1 = {
-        'lat': '37.7749',
-        'lon': '-122.4194',
-        'popup': 'san franscisco',
-    }
-    cities.append(city1)
-    city2 = {
-        'lat': '19.0760',
-        'lon': '72.8777',
-        'popup': 'mumbai',
-    }
-    cities.append(city2)
-    city3 = {
-        'lat': '39.9042',
-        'lon': '116.4074',
-        'popup': 'beijing',
-    }
-    cities.append(city3)
-    # return render(request, 'main/mainbase.html', {'cities': cities, 'form':form, 'weather_data' : weather_data})
-    return render(request, 'main/maptry.html', {'cities': cities})
-
-# registration login logout
-
+# registration 
 def sign_up(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
@@ -491,11 +281,11 @@ def sign_up(request):
 
     return render(request, 'registration/signup.html', {"form": form})
 
-# logout view
+# logout page
 def user_logout(request):
     logout(request)
     return redirect('/login')
 
-# about
+# about page
 def about(request):
     return render(request, 'main/about.html')
